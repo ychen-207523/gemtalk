553f4127ffe73db964f0dc946ab9a6252d175f6b
ychen-207523 <ychen207523@gmail.com>
2024-01-14 23:17:40 -0500

    add semantic ui

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// Loads all Semantic javascripts
+//= require semantic-ui
+//= require jquery
